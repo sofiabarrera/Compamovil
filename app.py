8e38d68dbc41c5823c654314fd6702960693ef12
JuanFelipe21 <juanfelipebahamon.c@gmail.com>
2023-05-24 16:12:06 -0500

    Com

--- app.py
+++ app.py
@@ -12,13 +12,25 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+#mysql=MySQL()
+#app.config['MYSQL_DATABASE_HOST']='localhost'
+#app.config['MYSQL_DATABASE_PORT']=3306
+#app.config['MYSQL_DATABASE_USER']='root'
+#app.config['MYSQL_DATABASE_PASSWORD']='1234'
+#app.config['MYSQL_DATABASE_DB']='usuario'
+#mysql.init_app(app)
+
 mysql=MySQL()
-app.config['MYSQL_DATABASE_HOST']='localhost'
-app.config['MYSQL_DATABASE_PORT']=3306
-app.config['MYSQL_DATABASE_USER']='root'
-app.config['MYSQL_DATABASE_PASSWORD']='1234'
-app.config['MYSQL_DATABASE_DB']='usuario'
+app.config['MYSQL_DATABASE_HOST']='db-mysql-nyc1-59254-do-user-14120281-0.b.db.ondigitalocean.com'
+app.config['MYSQL_DATABASE_PORT']=25060
+app.config['MYSQL_DATABASE_USER']='doadmin'
+app.config['MYSQL_DATABASE_PASSWORD']='AVNS_d7iL2uGXwTusSSqAHhP'
+app.config['MYSQL_DATABASE_DB']='defaultdb'
 mysql.init_app(app)
+
+
+
+
 
 @app.route('/')
 def index():
