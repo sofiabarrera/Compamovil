c793aa8f0f02e88a4c254703375d4cb586742047
JuanFelipe21 <juanfelipebahamon.c@gmail.com>
2023-05-25 15:26:33 -0500

    Si

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request
 from flask_session import Session
 from flaskext.mysql import MySQL
 from flask import send_from_directory
@@ -12,21 +12,17 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-#mysql=MySQL()
-#app.config['MYSQL_DATABASE_HOST']='localhost'
-#app.config['MYSQL_DATABASE_PORT']=3306
-#app.config['MYSQL_DATABASE_USER']='root'
-#app.config['MYSQL_DATABASE_PASSWORD']='1234'
-#app.config['MYSQL_DATABASE_DB']='usuario'
-#mysql.init_app(app)
-
 mysql=MySQL()
-app.config['MYSQL_DATABASE_HOST']='db-mysql-nyc1-59254-do-user-14120281-0.b.db.ondigitalocean.com'
-app.config['MYSQL_DATABASE_PORT']=25060
-app.config['MYSQL_DATABASE_USER']='doadmin'
-app.config['MYSQL_DATABASE_PASSWORD']='AVNS_d7iL2uGXwTusSSqAHhP'
-app.config['MYSQL_DATABASE_DB']='defaultdb'
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_PORT']=3306
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']='1234'
+app.config['MYSQL_DATABASE_DB']='usuario'
 mysql.init_app(app)
+
+
+
+
 
 
 
